e75f3d1a7ebb7c34c3c1eb9af63a3a86426cc70b
Lucas Weber <lucas.weber@fau.de>
2022-05-11 16:16:53 +0200

    [UP] add some tests

--- test/Test_FpGrowth.py
+++ test/Test_FpGrowth.py
@@ -11,6 +11,24 @@ import urllib.request
 import pandas as pd
 import requests
 import re
+import fpgrowth_py
+from csv import reader
+
+
+def get_from_csv(path='data.csv') -> list[list[str]]:
+    """
+    Function to load a csv file as a dataset
+    :param path: Path to the csv file
+    :return: the dataset in as a list of transactions where each transaction in a list of items as strings
+    """
+    data = []
+    with open(path, 'r') as file:
+        csv_reader = reader(file)
+        for line in csv_reader:
+            line = list(filter(None, line))
+            data.append(line)
+
+    return data
 
 
 def get_data() -> list[list]:
@@ -100,9 +118,6 @@ def get_mlxtend_result(dataset, min_support=0.5):
     #  make an assert statement about the support
     assert 0 <= min_support <= 1, f'[min_support] should be between 0 and 1. Currently it is [{min_support}].'
 
-    # make the counter
-    counter, number_of_transactions = count_items([list(set(transaction)) for transaction in dataset])
-
     # make the encoding
     te = TransactionEncoder()
     te_ary = te.fit(dataset).transform(dataset)
@@ -112,14 +127,7 @@ def get_mlxtend_result(dataset, min_support=0.5):
 
     result = mlxtend.frequent_patterns.fpgrowth(df, min_support=min_support, use_colnames=True)
 
-    # make frequent pattern dict from the result
-    frequent_patterns = {pattern['itemsets']: round(pattern['support']*number_of_transactions)
-                         for _, pattern in result.iterrows()}
-
-    # sort the frequent patterns
-    frequent_patterns = sort_frequent_pattern_names(frequent_patterns, counter)
-
-    return frequent_patterns
+    return result
 
 
 def test_own_algorithm(dataset: list[list], min_support=0.2, verbose=False):
@@ -135,17 +143,32 @@ def test_own_algorithm(dataset: list[list], min_support=0.2, verbose=False):
     #  make an assert statement about the support
     assert 0 <= min_support <= 1, f'[min_support] should be between 0 and 1. Currently it is [{min_support}].'
 
-    # get the reference result
+    # get the reference result -----------------------------------------------------------------------------------------
     timed = perf_counter()
     reference_result = get_mlxtend_result(dataset, min_support=min_support)
     ref_time = perf_counter() - timed
+    # make the counter
+    counter, number_of_transactions = count_items([list(set(transaction)) for transaction in dataset])
+    # make frequent pattern dict from the result
+    frequent_patterns = {pattern['itemsets']: round(pattern['support'] * number_of_transactions)
+                         for _, pattern in reference_result.iterrows()}
 
-    # get the result from own implementation
+    # sort the frequent patterns
+    reference_result = sort_frequent_pattern_names(frequent_patterns, counter)
+
+    # get the result from own implementation ---------------------------------------------------------------------------
     timed = perf_counter()
     own_result = fp_growth(dataset, min_support=min_support)
     own_time = perf_counter() - timed
 
-    # compare the two dicts
+    # get the result from Medium package -------------------------------------------------------------------------------
+    # https://towardsdatascience.com/fp-growth-frequent-pattern-generation-in-data-mining-with-python-implementation-244e561ab1c3
+    # https://github.com/chonyy/fpgrowth_py
+    timed = perf_counter()
+    second_result, _ = fpgrowth_py.fpgrowth(dataset, minSupRatio=min_support, minConf=0)
+    second_time = perf_counter() - timed
+
+    # compare reference with own implementation ------------------------------------------------------------------------
     if not reference_result == own_result:
         print('There is a difference between the results! The print goes from reference to own.')
         diff = deepdiff.DeepDiff(reference_result, own_result)
@@ -153,7 +176,8 @@ def test_own_algorithm(dataset: list[list], min_support=0.2, verbose=False):
         print(f'Own time: {own_time*1e3:0.2f} ms. Reference time: {ref_time*1e3:0.2f} ms.')
         raise ValueError('Implementation and reference are not the same.')
     else:
-        print(f'Everything works fine! Own time: {own_time*1e3:0.2f} ms. Reference time: {ref_time*1e3:0.2f} ms.')
+        print(f'Everything works fine! Own time: {own_time*1e3:0.2f} ms. Reference time: {ref_time*1e3:0.2f} ms.'
+              f' Second time: {second_time*1e3:0.2f} ms')
 
     # make a nice print of the results if requested
     if verbose:
@@ -177,7 +201,7 @@ def main():
                 ['Milk', 'Beer'],
                 ['Apple', 'Bananas']]
     dataset4 = get_KDD_dataset()
-    print(dataset2)
+    dataset5 = get_from_csv()
 
     # make a print to see when tests are started
     print('Tests will be starting: \n')
@@ -191,6 +215,7 @@ def main():
     test_own_algorithm(dataset3, min_support=0.2)
     test_own_algorithm(dataset4, min_support=0.02, verbose=True)
     test_own_algorithm(dataset4, min_support=0.005)
+    test_own_algorithm(dataset5, min_support=0.15)
 
 
 if __name__ == '__main__':
